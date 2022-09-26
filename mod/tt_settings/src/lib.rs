1a21dc9ef19f51e75e39901566e4267c8367b0ee
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-09-26 14:22:09 -0500

    Update lib.rs

--- mod/tt_settings/src/lib.rs
+++ mod/tt_settings/src/lib.rs
@@ -2,11 +2,23 @@ use pyo3::prelude::*;
 
 #[pymodule]
 fn tt_settings(_: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Keybind>()?;
     Ok(())
 }
 
 /// Struct for a Keybind
 #[pyclass]
 struct Keybind {
+    #[pyo3(get, set)]
+    key_id: String
+}
 
+#[pymethods]
+impl Keybind {
+
+    /// Create a new Keybind
+    #[new]
+    fn new(key_id:String) -> Self {
+        Self {key_id}
+    }
 }
