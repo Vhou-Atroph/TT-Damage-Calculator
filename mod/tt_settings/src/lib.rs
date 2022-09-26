cab3b06ba2caa11753121b0a67836dd806f8d7bd
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-09-26 15:23:16 -0500

    Update lib.rs

--- mod/tt_settings/src/lib.rs
+++ mod/tt_settings/src/lib.rs
@@ -1,3 +1,10 @@
+//! This is Rust module for the Toontown Damage Calculator. Its purpose is to help speed up operations performed by the program, which is written in Python.
+//! 
+//! BUILD INSTRUCTIONS:
+//! To build a tt_settings.pyd file, you must have Rust installed. If you don't have Rust installed, you can find the download link at <https://www.rust-lang.org/tools/install>.
+//! Once you have Rust installed, you need to navigate to the tt_settings file and run `cargo build --release` in your favorite terminal. Following this, navigate to target/releases and rename the generated tt_settings.dll file to tt_settings.pyd and place it in the "mod" folder of the main damage calculator.
+//! Make sure the program runs with any changes you have made!
+
 use std::fs;
 
 use pyo3::prelude::*;
