8a3988cda178dce13b136b64e3d9788675955b58
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-10-06 11:56:01 -0500

    Update lib.rs

--- mod/tt_settings/src/lib.rs
+++ mod/tt_settings/src/lib.rs
@@ -6,6 +6,7 @@ use std::fs;
 use pyo3::prelude::*;
 use serde::{Serialize, Deserialize};
 
+/// Module for managing settings, keybind functions, and handling the update checker.
 #[pymodule]
 fn tt_settings(_: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<Settings>()?;
