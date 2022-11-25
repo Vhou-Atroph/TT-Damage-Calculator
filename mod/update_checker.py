00aef17b26d0f649770956f170aafc7854544888
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-11-25 13:33:43 -0600

    Update update_checker.py
    :skull: whoops

--- mod/update_checker.py
+++ mod/update_checker.py
@@ -17,7 +17,7 @@ def compare_versions(local_file):
   if data[0]:
     messagebox.showinfo("Latest Version", "You are using the latest version of the Toontown Gag Calculator.")
   else:
-    diff_version(data[1],data[2])
+    diff_version(data[2],data[1])
 
 def diff_version(local,git):
   popup=Toplevel()
@@ -25,11 +25,11 @@ def diff_version(local,git):
   popup.resizable(0,0)
   popup_label=Label(popup,text="A new version may be available!",font=('Arial',11,'normal'))
   popup_label.grid(pady=6,padx=4,row=0,columnspan=2)
-  cur_ver_label=Label(popup,text="Current Version:",font=('Arial',11,'bold'))
+  cur_ver_label=Label(popup,text="Your version:",font=('Arial',11,'bold'))
   cur_ver_label.grid(padx=7,row=1,column=0)
   cur_ver_id_label=Label(popup,text=local,font=('Arial',10,'italic'))
   cur_ver_id_label.grid(row=2,column=0)
-  new_ver_label=Label(popup,text="GitHub Version:",font=('Arial',11,'bold'))
+  new_ver_label=Label(popup,text="Latest Version:",font=('Arial',11,'bold'))
   new_ver_label.grid(padx=7,row=1,column=1)
   new_ver_id_label=Label(popup,text=git,font=('Arial',10,'italic'))
   new_ver_id_label.grid(row=2,column=1)
