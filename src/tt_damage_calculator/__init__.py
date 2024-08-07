e7d76a8e205b9586c315a6cfe173e37e46de3d97
Vhou-Atroph <vhouatroph@proton.me>
2024-08-07 13:45:36 -0500

    Library
    Adding feature: TT-Damage-Calculator as a library
    ive wanted to do this for a little while, it could be useful for things like discord bots or other projects!

--- src/tt_damage_calculator/__init__.py
+++ src/tt_damage_calculator/__init__.py
@@ -5,8 +5,11 @@ Copyright (C) 2022-2024 Vhou-Atroph
 Run the Toontown Damage Calculator with python -m tt_damage_calculator instead!
 """
 
-__version__ = "4.3.0"
+__version__ = "4.3.2"
 
+# Imports for library functionality
+from .tt_damage_calculator import Gag, cog_hp, gag_calculator, full_calc, lvl_ind
+# Imports for app functionality
 from .widgets import App
 
 def main():
